2280f3521b2d0f0160c49012f08914a3981c83c8
Rodney Hammad <rodney.hammad@gmail.com>
2019-01-28 22:36:54 +0200

    Update renderer.rb

--- lib/rodcode_view_tool/renderer.rb
+++ lib/rodcode_view_tool/renderer.rb
@@ -1,7 +1,7 @@
 module RodcodeViewTool
     class Renderer
       def self.copyright name, msg
-        "&copy; #{Time.now.year} <b>#{name}</b> #{msg}".html_safe
+        "&copy; 2017 #{Time.now.year} <b>#{name}</b> #{msg}".html_safe
       end
     end
 end
